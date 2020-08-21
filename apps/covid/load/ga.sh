bbda9e0550c1b0c03f76971a12767734a350def0
David W. Streever <david@streever.com>
2020-08-21 16:47:57 -0400

    dir fix

--- apps/covid/load/ga.sh
+++ apps/covid/load/ga.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cd `dirname $0`
+CURR_DIR=`pwd`
 
 INPUT=$1
 PROCESSING_CYCLE=${INPUT:-`date +%Y-%m-%d`}
@@ -17,7 +18,7 @@ hdfs dfs -mkdir -p $HDFS_BASE_DIR/landing_zone
 
 wget --backups=3 -P $HOME/datasets/covid/ga https://ga-covid19.ondemand.sas.com/docs/ga_covid_data.zip
 
-cd $HOME/datasets/covid/ga
+pushd $HOME/datasets/covid/ga
 unzip -o ga_covid_data.zip
 
 #hdfs dfs -put -f countycases.csv /warehouse/tablespace/external/hive/covid_ga.db/COUNTY_CASES
@@ -31,5 +32,7 @@ hdfs dfs -put -f deaths.csv $HDFS_BASE_DIR/landing_zone
 #hdfs dfs -put -f demographics.csv /warehouse/tablespace/external/hive/covid_ga.db/DEMOGRAPHICS
 hdfs dfs -put -f demographics.csv $HDFS_BASE_DIR/DEMOGRAPHICS/${PROCESSING_CYCLE}_demographics.csv
 hdfs dfs -put -f demographics.csv $HDFS_BASE_DIR/landing_zone
+
+popd
 
 hive --hivevar DATABASE=${DATABASE} --hivevar PROCESSING_CYCLE=${PROCESSING_CYCLE} --hivevar HDFS_BASE_DIR=${HDFS_BASE_DIR} --hivevar ARCHIVE_PARTITION=${ARCHIVE_PARTITION} -f ../dml/ga_load.sql
